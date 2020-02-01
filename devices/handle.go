c9fe614c8e1c37c14f41109a3c1d1288b2eb5c2e
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2020-02-01 19:34:09 +1100

    Add documentation for device handles

--- devices/handle.go
+++ devices/handle.go
@@ -9,6 +9,9 @@ import (
 	"github.com/google/gopacket/pcap"
 )
 
+// DeviceHandle is an implementation of the gopacket PacketDataSource.
+// A custom handle is used so that we can stub out different device types
+// across platforms.
 type DeviceHandle interface {
 	gopacket.PacketDataSource
 
@@ -16,6 +19,7 @@ type DeviceHandle interface {
 	Close()
 }
 
+// OpenPcap opens a DeviceHandle for a live PCAP session on a given interface.
 func OpenPcap(device string, filter string, timeout time.Duration) (DeviceHandle, error) {
 	h, err := pcap.OpenLive(device, 1600, true, timeout)
 	if err != nil {
@@ -31,6 +35,7 @@ func OpenPcap(device string, filter string, timeout time.Duration) (DeviceHandle
 	return h, nil
 }
 
+// OpenFile opens a DeviceHandle for an offline PCAP session with a given input file.
 func OpenFile(file string, filter string) (DeviceHandle, error) {
 	h, err := pcap.OpenOffline(file)
 	if err != nil {
@@ -46,6 +51,12 @@ func OpenFile(file string, filter string) (DeviceHandle, error) {
 	return h, nil
 }
 
+// OpenAFPacket opens a DeviceHandle for live capture via AF_Packet on a given interface.
+// The buffer size depends on system memory, with frame and block sizes calculated from
+// the size of the buffer, system page size, and a default snaplen of 1600. Generally
+// multiples of 25MB are a good size, since 1600 bytes is an even multiple when page sizes
+// are in powers of 2. By default, 128 blocks can fit in 25MB. AF_Packet is only available
+// on Linux systems.
 func OpenAFPacket(device string, filter string, bufferSize int, timeout time.Duration) (DeviceHandle, error) {
 	frameSize, blockSize, blockCount, err := afpacketCalculateBuffers(bufferSize, 1600, os.Getpagesize())
 	if err != nil {
