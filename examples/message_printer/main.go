0c059ebd99ce164da883eea6e3048d65035b1eae
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2020-02-01 18:41:31 +1100

    Update message printer example for new sniffer interface

--- examples/message_printer/main.go
+++ examples/message_printer/main.go
@@ -17,26 +17,36 @@ func main() {
 	signal.Notify(gracefulStop, syscall.SIGTERM, syscall.SIGINT)
 
 	// Load inputs
-	var inet = flag.String("i", "", "The network interface to capture from")
+	var mode = flag.String("m", "pcap", "The sniffer source mode")
+	var inet = flag.String("i", "en0", "The network interface to capture from")
 	var file = flag.String("f", "", "The file path to capture from")
 
 	flag.Parse()
 
 	// Setup the Sniffer
-	sniffer, err := zanarkand.NewSniffer(*file, *inet)
+	var src string
+	switch *mode {
+	case "file":
+		src = *file
+	case "pcap":
+	default:
+		src = *inet
+	}
+
+	sniffer, err := zanarkand.NewSniffer(*mode, src)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Close when we're done
-	defer func() {
+	defer func(sniffer *zanarkand.Sniffer) {
 		if sniffer.Active() {
 			sniffer.Stop()
 			fmt.Println("stopped active sniifer")
 		} else {
 			fmt.Println("no active sniffer")
 		}
-	}()
+	}(sniffer)
 
 	// Create our message receiver channel
 	subscriber := zanarkand.NewGameEventSubscriber()
@@ -51,7 +61,7 @@ func main() {
 
 		case sig := <-gracefulStop:
 			fmt.Printf("Received %v signal\n", sig)
-			os.Exit(0)
+			os.Exit(0) // this is bad and should be run in a wrapper function since it breaks defers but
 		}
 	}
 }
