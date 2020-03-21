b0f97dcb2fe2299635e3b2c6dd957313d7fd916f
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2020-03-22 03:45:34 +1100

    Prune an ineffectual assignment in Frame

--- frame.go
+++ frame.go
@@ -135,13 +135,11 @@ func isPrivate(ip net.IP) bool {
 		return true
 	}
 
-	private := false
-
 	_, private24BitBlock, _ := net.ParseCIDR("10.0.0.0/8")
 	_, private20BitBlock, _ := net.ParseCIDR("172.16.0.0/12")
 	_, private16BitBlock, _ := net.ParseCIDR("192.168.0.0/16")
 
-	private = private24BitBlock.Contains(ip) || private20BitBlock.Contains(ip) || private16BitBlock.Contains(ip)
+	private := private24BitBlock.Contains(ip) || private20BitBlock.Contains(ip) || private16BitBlock.Contains(ip)
 
 	return private
 }
